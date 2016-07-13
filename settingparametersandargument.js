36adfc7f4fece3fb7838d200bfa9a62bd731a11f
Regine <rjbertille@gmail.com>
2016-07-13 05:03:15 +0200

    Add settingparametersandargument.js

---
+++ settingparametersandargument.js
@@ -0,0 +1,10 @@
+function addTwoNumbers(a, b) { //when i defining a FUNCTION, here what I'm defining here a and b
+	var result =  a + b;
+	alert(result);
+}
+
+addTwoNumbers(5,10);//when I'm calling the function, I'm passing in 5 and 10. There are my ARGUMENTS
+addTwoNumbers(67,10);
+addTwoNumbers(98,45);
+addTwoNumbers(23,94);
+
