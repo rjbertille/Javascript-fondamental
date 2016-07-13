fdced616ae48b69612a98d7a0f06cee6c3b99cb5
Regine <rjbertille@gmail.com>
2016-07-13 04:16:38 +0200

    Add whitespace.js

---
+++ whitespace.js
@@ -0,0 +1,14 @@
+var a;
+a = 5;
+a = 10000000;
+a = 123.654;
+a = -500;
+alert(a);
+
+var b  = 123;
+var c = "123";
+var x = true;
+var z = false;
+var namme = prompt("what is your name?");
+var message = "Hello";
+alert(message + name);
