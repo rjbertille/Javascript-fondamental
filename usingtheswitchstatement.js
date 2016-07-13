ccac261308af0d9a39cca1a2fd1040341924caef
Regine <rjbertille@gmail.com>
2016-07-13 04:49:05 +0200

    Add usingtheswitchstatement.js

---
+++ usingtheswitchstatement.js
@@ -0,0 +1,22 @@
+//The basics of a switch statement
+
+var grade = "Premium";
+
+switch (grade) {
+	case "Regular":
+	    alert("It's $3.15");
+	    break;
+
+	case "Premium":
+	     alert("It's $3.35");
+	     break;
+
+	case "Diesel":
+	     alert("It's $3.47");
+	     break;
+
+	default:
+	      alert("That's not a valid grade");
+
+      
+}
